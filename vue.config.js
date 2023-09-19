a630d7a933bb156944cad5a6cbddaf028fc05bbf
wangkangzhang <1368504885@qq.com>
2023-09-19 11:11:05 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,24 @@
 'use strict';
 const path = require('path');
-function resolve(dir) {
+
+function resolve (dir) {
   return path.join(__dirname, dir);
 }
+
 module.exports = {
-  publicPath: "./",
-  devServer: { },
-  chainWebpack(config) {
+  // publicPath: "./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/yian/' : '/',
+  devServer: {},
+  chainWebpack (config) {
     config.module
       .rule('svg')
-      .exclude.add(resolve('src/common/icons'))       //注意svg的存储地址
+      .exclude.add(resolve('src/common/icons')) // 注意svg的存储地址
       .end();
     config.module
       .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve('src/common/icons'))        //注意svg的存储地址
+      .include.add(resolve('src/common/icons')) // 注意svg的存储地址
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
