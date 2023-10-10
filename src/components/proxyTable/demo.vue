baf2c797b4f24ce214577eb8cb7d2b6918e445c5
wangkangzhang <1368504885@qq.com>
2023-10-10 10:20:12 +0800

    更新

--- src/components/proxyTable/demo.vue
+++ src/components/proxyTable/demo.vue
@@ -6,6 +6,8 @@
       table-title="表格标题（注：表中数据均为MockJs随机生成的数据）"
       :columns="columns"
       :operations="operations"
+      :pageSize="50"
+      :total="tableData.length"
       :isCheckboxFixed="true"
       :is-index-fixed="true"
       :diy-get-list="diyGetList"
@@ -148,7 +150,7 @@ export default {
     // const Random = this.$Mock.Random;
     const data = this.$Mock.mock({
       // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-      'list|100': [{
+      'list|500': [{
         // 属性 id 是一个自增数，起始值为 1，每次增 1
         'id|+1': 1,
         name: '@cname',
--- src/components/proxyTable/index.vue
+++ src/components/proxyTable/index.vue
@@ -201,12 +201,12 @@
           popper-class="custom-pagination"
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="1"
+          :current-page="currentPageNum"
           :pager-count="5"
           :page-sizes="[20, 50, 100, 200]"
-          :page-size="100"
+          :page-size.sync="pageSizeNum"
           layout="total, prev, pager, next, sizes, jumper"
-          :total="400">
+          :total="total">
       </el-pagination>
     </div>
   </div>
@@ -214,6 +214,7 @@
 
 <script>
 import tableColumn from './table-column/index.vue';
+import props from './props';
 
 export default {
   name: 'index',
@@ -222,6 +223,8 @@ export default {
   },
   data () {
     return {
+      currentPageNum: 1,
+      pageSizeNum: 20,
       checkList: [],
       filterCheckList: [],
       searchValue: '',
@@ -256,181 +259,7 @@ export default {
     };
   },
   props: {
-    /* 是否显示自定义表格顶部工具栏插槽 */
-    isShowSlotToolBar: {
-      type: Boolean,
-      default: false
-    },
-    /* 是否显示搜索输入框 */
-    isShowSearchInput: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否显示默认搜索工具栏 */
-    isShowToolBar: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否显示默认工具栏 */
-    isShowDefaultToolBar: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否显示分页 */
-    isShowPagination: {
-      type: Boolean,
-      default: true
-    },
-    /* 表格标题 */
-    tableTitle: {
-      type: String,
-      default: ''
-    },
-    /* 搜索框提示语 */
-    placeholder: {
-      type: String,
-      default: '请输入'
-    },
-    /* 搜索按钮文字 */
-    searchBtnLabel: {
-      type: String,
-      default: '搜索'
-    },
-    /* 表格数据 */
-    tableData: {
-      type: Array,
-      default: () => []
-    },
-    /* 表格顶部功能按钮数据 */
-    operations: {
-      type: Array,
-      default: () => []
-    },
-    /* 列数据 */
-    columns: {
-      type: Array,
-      default: () => []
-    },
-    /* 表格列表按钮 */
-    tableBtns: {
-      type: Array,
-      default: () => []
-    },
-    /* 是否显示边框 */
-    border: {
-      type: Boolean,
-      default: false
-    },
-    /* 是否显示筛选，这里的筛选是筛选列的显示 */
-    isShowFilter: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否斑马条纹 */
-    stripe: {
-      type: Boolean,
-      default: false
-    },
-
-    /* 是否懒加载子节点数据 */
-    lazy: {
-      type: Boolean,
-      default: true
-    },
-    /* 加载子节点数据的函数 */
-    load: {
-      type: Function
-    },
-    /* Table 的尺寸 */
-    size: {
-      type: String,
-      default: 'small'
-    },
-    /* 是否显示表头 */
-    showHeader: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否要高亮当前行 */
-    highlightCurrentRow: {
-      type: Boolean,
-      default: true
-    },
-    /* 表尾合计 */
-    showSummary: {
-      type: Boolean,
-      default: false
-    },
-    /* 表尾统计文案 */
-    sumText: {
-      type: String,
-      default: '合计'
-    },
-    /* 自定义合计规则 */
-    getSummaries: {
-      type: Function
-    },
-    /* 自定义排序规则 */
-    sortMethod: {
-      type: Function
-    },
-
-    /* 表格行、列合并 */
-    spanMethod: {
-      type: Function
-    },
-    /* 是否显示多选框 */
-    showCheckbox: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否显示序号 */
-    showIndex: {
-      type: Boolean,
-      default: true
-    },
-    /* 是否固定多选框 */
-    isCheckboxFixed: {
-      type: Boolean,
-      default: false
-    },
-    /* 是否固定序号 */
-    isIndexFixed: {
-      type: Boolean,
-      default: false
-    },
-    /* 是否超出隐藏浮现 */
-    showOverflowTooltip: {
-      type: Boolean,
-      default: true
-    },
-    /* 自定义操作列 */
-    slotOperation: {
-      type: Boolean,
-      default: false
-    },
-    /* 文字按钮 */
-    isShowTextBtn: {
-      type: Boolean,
-      default: true
-    },
-    /* 自定义多选框可选 */
-    diyHasCheckBox: {
-      type: Function
-    },
-    /* 自定义序号 */
-    diyIndexMethod: {
-      type: Function
-    },
-    /* 查询函数 */
-    diyGetList: {
-      type: Function
-    },
-
-    /* 表头样式自定义 */
-    renderHeader: {
-      type: Function
-    }
+    ...props
   },
   watch: {
     table: {
@@ -446,6 +275,22 @@ export default {
         if (newVal && newVal.length > 0) {
           this.checkList = newVal.map(item => item.prop);
           this.filterCheckList = this.checkList;
+        }
+      },
+      immediate: true
+    },
+    currentPage: {
+      handler (newVal) {
+        if (newVal !== this.currentPageNum) {
+          this.currentPageNum = newVal;
+        }
+      },
+      immediate: true
+    },
+    pageSize: {
+      handler (newVal) {
+        if (newVal !== this.pageSizeNum) {
+          this.pageSizeNum = newVal;
         }
       },
       immediate: true
@@ -479,6 +324,8 @@ export default {
      */
     handleSizeChange (val) {
       console.log('sizeVal', val);
+      this.pageSizeNum = val;
+      this.$emit('size-change', val);
     },
     /**
      * @Description 切换页码触发
@@ -487,6 +334,8 @@ export default {
      */
     handleCurrentChange (val) {
       console.log('pageVal', val);
+      this.currentPageNum = val;
+      this.$emit('current-change', val);
     },
     /**
      * @Description 复选框
